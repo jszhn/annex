9b2f47ea963604e56ca34e80e6550a2ca64b0f4c
Jason Zhang <zhangj1079@gmail.com>
2024-08-30 21:52:07 -0400

    IO functions

---
+++ src/syntax/io.rs
@@ -0,0 +1,8 @@
+use crate::syntax::Ast;
+use std::fmt;
+
+impl fmt::Display for Ast {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        todo!();
+    }
+}
--- src/token/io.rs
+++ src/token/io.rs
@@ -1,22 +1,35 @@
-use crate::token::{CToken, CTokenType};
+use crate::token::{Token, TokenType};
+use std::fs;
 
-pub fn print_tokens(vec: &Vec<CToken>) {
+pub fn print_tokens(vec: &Vec<Token>) {
     // only prints tokens
     for item in vec {
         println!("{}", item.value);
     }
 }
 
-pub fn print_all(vec: &Vec<CToken>) {
+pub fn print_all(vec: &Vec<Token>) {
     // prints all: enum value and token
     for item in vec {
         println!("{}: {}", get_token_name(item.token_type), item.value);
     }
 }
 
-impl crate::util::OutputHandler for Vec<CToken> {
+impl crate::util::OutputHandler for Vec<Token> {
     fn to_txt(&self, path: String) -> bool {
-        todo!()
+        let mut output: String = String::from("");
+        for item in self {
+            let mut buf = get_token_name(item.token_type);
+            buf.push_str(": ");
+            buf.push_str(item.value.as_str());
+            buf.push_str("\n");
+            output.push_str(buf.as_str());
+        }
+        if let Ok(()) = fs::write(path, output) {
+            true
+        } else {
+            false
+        }
     }
 
     fn from_txt(&self, path: String) {
@@ -24,19 +37,19 @@ impl crate::util::OutputHandler for Vec<CToken> {
     }
 }
 
-fn get_token_name(_type: CTokenType) -> String {
+fn get_token_name(_type: TokenType) -> String {
     // to avoid needing the debug specifier
     match _type {
-        CTokenType::Type => "Type".to_string(),
-        CTokenType::Control => "Control".to_string(),
-        CTokenType::Specifier => "Specifier".to_string(),
-        CTokenType::Operator => "Operator".to_string(),
-        CTokenType::Identifier => "Identifier".to_string(),
-        CTokenType::Separator => "Separator".to_string(),
-        CTokenType::Return => "Return".to_string(),
-        CTokenType::Boolean => "Boolean".to_string(),
-        CTokenType::GroupBegin => "GroupBegin".to_string(),
-        CTokenType::GroupEnd => "GroupEnd".to_string(),
+        TokenType::Type => "Type".to_string(),
+        TokenType::Control => "Control".to_string(),
+        TokenType::Specifier => "Specifier".to_string(),
+        TokenType::Operator => "Operator".to_string(),
+        TokenType::Identifier => "Identifier".to_string(),
+        TokenType::Separator => "Separator".to_string(),
+        TokenType::Return => "Return".to_string(),
+        TokenType::Boolean => "Boolean".to_string(),
+        TokenType::GroupBegin => "GroupBegin".to_string(),
+        TokenType::GroupEnd => "GroupEnd".to_string(),
         _ => "None".to_string(),
     }
 }
